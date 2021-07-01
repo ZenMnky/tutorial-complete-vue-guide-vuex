d6c716b274dac493aeb3498f11e8fe015136eec4
Justin Hager <developerjustinhager@pm.me>
2021-07-01 11:25:35 -0700

    set up some getters and mutataions

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>Count: {{ counter }}</h3>
+    <button @click="addValue">Add 10</button>
+    <button @click="clearCounter">Reset</button>
+  </base-container>
+  <FavoriteValue />
+  <NormalizedCounter />
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+import NormalizedCounter from './components/NormalizedCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    FavoriteValue,
+    NormalizedCounter
   },
+  methods: {
+    addValue() {
+      // this.$store.commit('increase', { value: 10 });
+
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      });
+    },
+    clearCounter() {
+      this.$store.commit('clearCount');
+    }
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  }
 };
 </script>
 
 <style>
 * {
   box-sizing: border-box;
+  text-align: center;
 }
 
 html {
@@ -23,5 +52,8 @@ html {
 
 body {
   margin: 0;
+}
+button {
+  margin: 0 5px 0 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+    clearCount(state) {
+      state.counter = 0;
+    }
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    }
+  }
+});
+
 const app = createApp(App);
 
+app.use(store);
 app.mount('#app');
